f2855c65b317c454ad2226fbb4672c518856b737
biswaljyotirmaya <jyotirmayabiswal03@gmail.com>
2025-12-06 16:57:04 +0530

    Initial Commit

--- public/firebase.js
+++ public/firebase.js
@@ -1,12 +1,22 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyChv7GrjKKo8Z8qoKySRS-EB5_ewTb0mLg",
+//     authDomain: "propertymeasure-12320.firebaseapp.com",
+//     databaseURL: "https://propertymeasure-12320-default-rtdb.firebaseio.com",
+//     projectId: "propertymeasure-12320",
+//     storageBucket: "propertymeasure-12320.firebasestorage.app",
+//     messagingSenderId: "348835277099",
+//     appId: "1:348835277099:web:96dc4a603e50a6ceabfd6e",
+//     measurementId: "G-LK15LFXW8C"
+// };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyChv7GrjKKo8Z8qoKySRS-EB5_ewTb0mLg",
-    authDomain: "propertymeasure-12320.firebaseapp.com",
-    databaseURL: "https://propertymeasure-12320-default-rtdb.firebaseio.com",
-    projectId: "propertymeasure-12320",
-    storageBucket: "propertymeasure-12320.firebasestorage.app",
-    messagingSenderId: "348835277099",
-    appId: "1:348835277099:web:96dc4a603e50a6ceabfd6e",
-    measurementId: "G-LK15LFXW8C"
+    apiKey: "AIzaSyAf1nNvmjShoBP8gj4Ku2OtF3ViWxtAk0U",
+    authDomain: "propertymeasuredemo.firebaseapp.com",
+    databaseURL: "https://propertymeasuredemo-default-rtdb.firebaseio.com",
+    projectId: "propertymeasuredemo",
+    storageBucket: "propertymeasuredemo.firebasestorage.app",
+    messagingSenderId: "649352054528",
+    appId: "1:649352054528:web:f51534682d900820f715e5"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
